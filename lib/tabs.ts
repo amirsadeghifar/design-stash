1b28a037f44e4518982a3f0983b8d5c420b9e349
Amir Sadeghifar <amfar77@gmail.com>
2026-03-06 02:32:08 -0500

    initial commit

---
+++ lib/tabs.ts
@@ -0,0 +1,55 @@
+export const TABS = [
+  {
+    title: 'Web',
+    subtitle: 'Crafting Modern Interfaces',
+    links: [
+      { label: 'Shadcn', url: 'https://ui.shadcn.com/' },
+      { label: 'Motion Primitives', url: 'https://motion-primitives.com/docs' },
+      { label: 'React Bits', url: 'https://www.reactbits.dev/' },
+      { label: 'Magic UI', url: 'https://magicui.design/' },
+      { label: 'Motion', url: 'https://motion.dev//' },
+      { label: 'Anime JS', url: 'https://animejs.com/' },
+    ],
+  },
+  {
+    title: 'Mobile',
+    subtitle: 'Designing for Every Screen',
+    links: [
+      { label: 'Expo', url: 'https://expo.dev' },
+      { label: 'Reacticx', url: 'https://www.reacticx.com/' },
+      { label: 'React Native Reanimated', url: 'https://docs.swmansion.com/react-native-reanimated/' },
+      { label: 'React Native Reusables', url: 'https://reactnativereusables.com/' },
+    ],
+  },
+  {
+    title: 'Design',
+    subtitle: 'From Concept to Pixel',
+    links: [
+      { label: 'Paper', url: 'https://paper.design/' },
+      { label: 'Craftwork', url: 'https://craftwork.design/' },
+      { label: 'Josh Puckett', url: 'https://joshpuckett.me/' },
+      { label: 'Interface Craft', url: 'https://www.interfacecraft.dev/' },
+      { label: 'Dribbble', url: 'https://dribbble.com/' },
+      { label: 'Mobbin', url: 'https://mobbin.com' },
+    ],
+  },
+  {
+    title: 'AI',
+    subtitle: 'Intelligent Design Workflows',
+    links: [
+      { label: 'Variant', url: 'https://variant.com/' },
+      { label: 'UI Skills', url: 'https://www.ui-skills.com/' },
+    ],
+  },
+  {
+    title: 'Showcase',
+    subtitle: 'Inspiring Builds',
+    links: [
+      { label: 'Extend', url: 'https://extend.ai/' },
+      { label: 'Attio', url: 'https://attio.com/' },
+      { label: 'Reducto', url: 'https://www.reducto.ai/' },
+      { label: 'Wisprflow', url: 'https://wisprflow.ai/' },
+      { label: 'Jitter', url: 'https://jitter.video/' },
+    ],
+  },
+];
